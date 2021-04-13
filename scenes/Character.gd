022372aca650929b1ee6f71a048a580f4af24c26
Francisco Esquibel <f.esquibelalvarez@gmail.com>
2021-04-13 17:08:16 -0400

    Adding character outline

--- scenes/Character.gd
+++ scenes/Character.gd
@@ -1,16 +1,37 @@
 extends Area2D
 
-var picked : bool = false
+signal selected
+signal released(charc, pos)
 
-func _input_event(viewport, event, shape_idx):
+var picked : bool = false
+var select : bool = false
+var initial_pos := Vector2.ZERO
+
+onready var animation := $AnimatedSprite
+
+func _input_event(_viewport, event, _shape_idx) -> void:
 	if event is InputEventMouseButton:
 		if !picked:
 			picked = true
+			initial_pos = global_position
+			emit_signal("selected", self)
 			print("picked")
 		else:
 			picked = false
+			emit_signal("released", self, global_position)
 			print("released")
 
-func _process(delta):
+func _process(_delta) -> void:
 	if picked:
 		global_position = get_global_mouse_position()
+
+func return_initial_pos() -> void:
+	global_position = initial_pos
+
+func unselect() -> void:
+	select = false
+	animation.frame = 0
+
+func select() -> void:
+	select = true
+	animation.frame = 1
--- scenes/Main.gd
+++ scenes/Main.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
+var selected_character : Area2D = null
+var player_turn := false
+var player_char_count : int
+
 onready var hint : Node2D = $Hint
 onready var tilemap : TileMap = $TileMap 
-onready var character : Area2D = $Character
+onready var characters : Node2D = $Characters
 
 func _ready() -> void:
-	pass
+	player_char_count = characters.get_children().size()
+	for c in characters.get_children():
+		c.connect("selected", self, "_on_Character_selected")
+		c.connect("released", self, "_on_Character_released")
 
 func _process(_delta : float) -> void:
 	var mouse_pos : Vector2 = get_global_mouse_position()
@@ -17,16 +24,30 @@ func _process(_delta : float) -> void:
 #		print("cell: ", cell)
 #		if cell != -1:
 #			character.global_position = tilemap.map_to_world(tile_pos)
-	
 	if cell != -1:
 		hint.show()
 		hint.global_position = tilemap.map_to_world(tile_pos)
 	else:
 		hint.hide()
 	
-
 func car_to_iso(pos : Vector2) -> Vector2:
 	return Vector2(pos.x + pos.y, 0.5 * (pos.y - pos.x))
 
 func iso_to_car(pos : Vector2) -> Vector2:
 	return Vector2(0.5 * (pos.x - pos.y), pos.y + pos.x)
+
+func _on_Character_released(charc, pos) -> void:
+	var tile_pos := tilemap.world_to_map(pos)
+	var cell = tilemap.get_cell(tile_pos.x - 11, tile_pos.y - 1)
+	var char_pos := tilemap.map_to_world(tile_pos)
+	if cell != -1:
+		charc.global_position = Vector2(char_pos.x, char_pos.y + 12)
+	else:
+		charc.return_initial_pos()
+
+func _on_Character_selected(charc) -> void:
+	if selected_character:
+		selected_character.unselect()
+	selected_character = charc
+	selected_character.select()
+	
